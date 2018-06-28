bdf7142adb73dfdcd96a098e0ad67b6babde837e
Radoslaw Cieciwa <radoslaw.cieciwa@corp.badoo.com>
2018-06-28 15:17:32 +0100

    Fixed the install script.

--- install.sh
+++ install.sh
@@ -21,17 +21,19 @@ EOF
 _generate_post_commit() {
   FILE_NAME=$1
   echo "#!/bin/sh"
-  echo "git log -1 --date=short --pretty="%s [%cd]" >> $FILE_NAME"
+  echo "WORK=\`git log -1 --date=short --pretty=\"%s [%cd]\"\`"
+  echo "echo \"[Local]: Logged your work! < \$WORK >\""
+  echo "echo \$WORK >> $FILE_NAME"
 }
 
 _execute() {
   POST_COMMIT_HOOK_PATH="$1/.git/hooks/post-commit"
   CURRENT_PATH=`pwd`
-  LOG_FILE="$2.log"
+  LOG_FILE="$CURRENT_PATH/$2.log"
   POST_COMMIT_SCRIPT="$CURRENT_PATH/.post_commit_$2"
   _generate_post_commit $LOG_FILE > $POST_COMMIT_SCRIPT
   chmod u+x $POST_COMMIT_SCRIPT
-  touch "$CURRENT_PATH/$LOG_FILE"
+  touch $LOG_FILE
   ln -s -f $POST_COMMIT_SCRIPT $POST_COMMIT_HOOK_PATH
 }
 
